566cce9ab493cb1e89ed8040254586d13c70d69f
Lance Pollard <lancejpollard@gmail.com>
2014-11-12 12:26:47 -0800

    add idea of scene

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 
-exports.node = require('./lib/node');
-exports.component = require('./lib/component');
+/**
+ * Module dependencies.
+ */
+
+var component = require('./lib/component');
+var scene = require('./renderer/scene');
+var node = require('./lib/node');
+
+/**
+ * Expose `Scene`.
+ */
+
+exports.scene = scene;
+
+/**
+ * Expose `component`.
+ */
+
+exports.component = component;
+
+/**
+ * Expose `node`.
+ */
+
+exports.node = node;
