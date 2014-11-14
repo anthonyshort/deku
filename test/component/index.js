195b868ad00d02fb4416f86f4cf6137966fbfff0
Anthony Short <antshort@gmail.com>
2014-11-13 20:43:52 -0800

    Rendered when state changes
    * Added initialState to get the intial state for component
    * When state changes, the component is re-rendered
    * Allow strings as the children to the dom() method

--- test/component/index.js
+++ test/component/index.js
@@ -13,6 +13,16 @@ describe('component', function(){
     assert.equal(el.innerHTML, '<span>Hello World</span>');
   });
 
+  it('should allow strings as children', function(){
+    var Page = component({
+      render: function(dom, state, props) {
+        return dom('span', null, 'Hello World');
+      }
+    });
+    Page.render(el);
+    assert.equal(el.innerHTML, '<span>Hello World</span>');
+  });
+
   it('should render nothing visible on the page by default', function(){
     var Page = component();
     Page.render(el);
@@ -33,40 +43,73 @@ describe('component', function(){
   });
 
   it('should render sub-components', function(){
-
     var ComponentA = component({
       render: function(n, state, props){
         return n('span', { name: props.name }, [props.text]);
       }
     });
-
     var ComponentB = component({
       render: function(n, state, props){
         return n(ComponentA, { text: 'foo', name: props.name });
       }
     });
-
     var mount = ComponentB.render(el, { name: 'Bob' });
     assert.equal(el.innerHTML, '<span name="Bob">foo</span>');
   });
 
   it('should update sub-components', function(){
-
     var ComponentA = component({
       render: function(n, state, props){
         return n('span', { name: props.name }, [props.text]);
       }
     });
-
     var ComponentB = component({
       render: function(n, state, props){
         return n(ComponentA, { text: 'foo', name: props.name });
       }
     });
-
     var mount = ComponentB.render(el, { name: 'Bob' });
     mount.set({ name: 'Tom' })
     assert.equal(el.innerHTML, '<span name="Tom">foo</span>');
+  });
+
+  it('should have initial state', function(done){
+    var ComponentA = component({
+      initialState: function(){
+        return {
+          duration: 0
+        }
+      },
+      render: function(dom, state){
+        assert(state.duration === 0);
+        done();
+      }
+    });
+    ComponentA.render(el);
+  });
+
+  it('should update when the component state changes', function(done){
+    var ComponentA = component({
+      initialState: function(){
+        return {
+          duration: 0
+        }
+      },
+      mount: function(){
+        var self = this;
+        setTimeout(function(){
+          self.set('duration', 1);
+        }, 1);
+      },
+      render: function(n, state, props){
+        return n('span', null, [state.duration]);
+      }
+    });
+    ComponentA.render(el);
+    setTimeout(function(){
+      assert.equal(el.innerHTML, '<span>1</span>');
+      done();
+    }, 2);
   });
 
 });
