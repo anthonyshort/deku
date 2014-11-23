415ce6ceeed2be90628f072755dcace5d2775687
Anthony Short <antshort@gmail.com>
2014-11-22 22:11:33 -0800

    Running all tests

--- test/component/stringify.js
+++ test/component/stringify.js
@@ -1,7 +1,7 @@
 var assert = require('component/assert@0.4.0');
 var component = require('/lib/component');
 
-describe.only('stringify', function(){
+describe('stringify', function(){
 
   it('should render an element', function () {
     var Component = component({
