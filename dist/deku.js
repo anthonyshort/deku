f45124fb96b43ed910f5a9f6d59ba43444c6f1d7
Anthony Short <antshort@gmail.com>
2015-02-03 18:55:19 -0800

    Release 0.0.22

--- dist/deku.js
+++ dist/deku.js
@@ -1,7 +1,7 @@
 !function(e){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=e();else if("function"==typeof define&&define.amd)define([],e);else{var f;"undefined"!=typeof window?f=window:"undefined"!=typeof global?f=global:"undefined"!=typeof self&&(f=self),f.deku=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof _require=="function"&&_require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof _require=="function"&&_require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(_require,module,exports){
 exports.component = _require('./lib/component');
-exports.dom = _require('./lib/virtual').node;
-},{"./lib/component":2,"./lib/virtual":13}],2:[function(_require,module,exports){
+exports.dom = _require('virtualize').node;
+},{"./lib/component":2,"virtualize":40}],2:[function(_require,module,exports){
 
 /**
  * Module dependencies.
@@ -4088,5 +4088,416 @@ function removeFromArray(item, array) {
   if (index === -1) return;
   array.splice(index, 1);
 }
-},{"component-emitter":22}]},{},[1])(1)
+},{"component-emitter":22}],40:[function(_require,module,exports){
+
+/**
+ * Module dependencies.
+ */
+
+var ComponentNode = _require('./lib/component');
+var ElementNode = _require('./lib/element');
+var TextNode = _require('./lib/text');
+var tree = _require('./lib/tree');
+var uid = _require('get-uid');
+
+/**
+ * Exports.
+ */
+
+exports.node = dom;
+exports.tree = tree;
+
+/**
+ * Create virtual DOM trees.
+ *
+ * This creates the nicer API for the user.
+ * It translates that friendly API into an actual tree of nodes.
+ *
+ * @param {String|Function} type
+ * @param {Object} props
+ * @param {Array} children
+ * @return {Node}
+ * @api public
+ */
+
+function dom(type, props, children) {
+
+  // Skipped adding attributes and we're passing
+  // in children instead.
+  if (arguments.length === 2 && (typeof props === 'string' || Array.isArray(props))) {
+    children = props;
+    props = {};
+  }
+
+  children = children || [];
+  props = props || {};
+
+  // passing in a single child, you can skip
+  // using the array
+  if (!Array.isArray(children)) {
+    children = [ children ];
+  }
+
+  children = children
+    .filter(notEmpty)
+    .reduce(flatten, [])
+    .map(textNodes)
+    .map(addIndex);
+
+  // pull the key out from the data.
+  var key = props.key;
+  delete props.key;
+
+  // if you pass in a function, it's a `Component` constructor.
+  // otherwise it's an element.
+  var node;
+  if ('function' == typeof type) {
+    node = new ComponentNode(type, props, key, children);
+  } else {
+    node = new ElementNode(type, props, key, children);
+  }
+
+  // set the unique ID
+  node.id = uid();
+  node.index = 0;
+
+  return node;
+}
+
+/**
+ * Remove null/undefined values from the array
+ *
+ * @param {*} value
+ *
+ * @return {Boolean}
+ */
+
+function notEmpty(value) {
+  return value !== null && value !== undefined;
+}
+
+/**
+ * Flatten nested array
+ *
+ * @param {Array} arr
+ * @param {*} value
+ *
+ * @return {Array}
+ */
+
+function flatten(arr, node) {
+  if (Array.isArray(node)) {
+    arr = arr.concat(node);
+  } else {
+    arr.push(node);
+  }
+  return arr;
+}
+
+/**
+ * Parse nodes into real `Node` objects.
+ *
+ * @param {Mixed} node
+ * @param {Integer} index
+ * @return {Node}
+ * @api private
+ */
+
+function textNodes(node, index) {
+  if (typeof node === 'string' || typeof node === 'number') {
+    return new TextNode(String(node));
+  } else {
+    return node;
+  }
+}
+
+/**
+ * Add an index
+ *
+ * @param {Node} node
+ * @param {Number} index
+ *
+ * @return {Node}
+ */
+
+function addIndex(node, index) {
+  node.index = index;
+  return node;
+}
+
+},{"./lib/component":41,"./lib/element":42,"./lib/text":43,"./lib/tree":44,"get-uid":27}],41:[function(_require,module,exports){
+
+module.exports = ComponentNode;
+
+/**
+ * Initialize a new `ComponentNode`.
+ *
+ * @param {Component} component
+ * @param {Object} props
+ * @param {String} key Used for sorting/replacing during diffing.
+ * @param {Array} children Child virtual nodes
+ * @api public
+ */
+
+function ComponentNode(component, props, key, children) {
+  this.key = key;
+  this.props = props;
+  this.type = 'component';
+  this.component = component;
+  this.props.children = children || [];
+}
+
+},{}],42:[function(_require,module,exports){
+var type = _require('component-type');
+
+/**
+ * Exports
+ */
+
+module.exports = ElementNode;
+
+/**
+ * All of the events can bind to
+ */
+
+var events = {
+  onBlur: 'blur',
+  onChange: 'change',
+  onClick: 'click',
+  onContextMenu: 'contextmenu',
+  onCopy: 'copy',
+  onCut: 'cut',
+  onDoubleClick: 'dblclick',
+  onDrag: 'drag',
+  onDragEnd: 'dragend',
+  onDragEnter: 'dragenter',
+  onDragExit: 'dragexit',
+  onDragLeave: 'dragleave',
+  onDragOver: 'dragover',
+  onDragStart: 'dragstart',
+  onDrop: 'drop',
+  onFocus: 'focus',
+  onInput: 'input',
+  onKeyDown: 'keydown',
+  onKeyUp: 'keyup',
+  onMouseDown: 'mousedown',
+  onMouseMove: 'mousemove',
+  onMouseOut: 'mouseout',
+  onMouseOver: 'mouseover',
+  onMouseUp: 'mouseup',
+  onPaste: 'paste',
+  onScroll: 'scroll',
+  onSubmit: 'submit',
+  onTouchCancel: 'touchcancel',
+  onTouchEnd: 'touchend',
+  onTouchMove: 'touchmove',
+  onTouchStart: 'touchstart'
+};
+
+/**
+ * Initialize a new `ElementNode`.
+ *
+ * @param {String} tagName
+ * @param {Object} attributes
+ * @param {String} key Used for sorting/replacing during diffing.
+ * @param {Array} children Child virtual dom nodes.
+ * @api public
+ */
+
+function ElementNode(tagName, attributes, key, children) {
+  this.type = 'element';
+  this.attributes = parseAttributes(attributes);
+  this.events = parseEvents(attributes);
+  this.tagName = parseTag(tagName, attributes);
+  this.children = children || [];
+  this.key = key;
+}
+
+/**
+ * Parse attributes for some special cases.
+ *
+ * TODO: This could be more functional and allow hooks
+ * into the processing of the attributes at a component-level
+ *
+ * @param {Object} attributes
+ *
+ * @return {Object}
+ */
+
+function parseAttributes(attributes) {
+
+  // style: { 'text-align': 'left' }
+  if (attributes.style) {
+    attributes.style = parseStyle(attributes.style);
+  }
+
+  // data: { foo: 'bar' }
+  if (attributes.data) {
+    attributes = parseData(attributes);
+  }
+
+  // class: { foo: true, bar: false, baz: true }
+  // class: ['foo', 'bar', 'baz']
+  if (attributes.class) {
+    attributes.class = parseClass(attributes.class);
+  }
+
+  // Remove attributes with false values
+  for (var name in attributes) {
+    if (attributes[name] === false) {
+      delete attributes[name];
+    }
+  }
+
+  return attributes;
+}
+
+/**
+ * Parse a block of styles into a string.
+ *
+ * TODO: this could do a lot more with vendor prefixing,
+ * number values etc. Maybe there's a way to allow users
+ * to hook into this?
+ *
+ * @param {Object} styles
+ *
+ * @return {String}
+ */
+
+function parseStyle(styles) {
+  if (type(styles) !== 'object') {
+    return styles;
+  }
+  var str = '';
+  for (var name in styles) {
+    var value = styles[name];
+    str += name + ':' + value + ';';
+  }
+  return str;
+}
+
+/**
+ * Parse the dataset
+ *
+ * @param {Object} attributes
+ *
+ * @return {Object}
+ */
+
+function parseData(attributes) {
+  if (type(attributes.data) !== 'object') {
+    return attributes;
+  }
+
+  for (var name in attributes.data) {
+    attributes['data-' + name] = attributes.data[name];
+  }
+
+  delete attributes.data;
+  return attributes;
+}
+
+/**
+ * Parse the class attribute so it's able to be
+ * set in a more user-friendly way
+ *
+ * @param {String|Object|Array} value
+ *
+ * @return {String}
+ */
+
+function parseClass(value) {
+
+  // { foo: true, bar: false, baz: true }
+  if (type(value) === 'object') {
+    var matched = [];
+    for (var key in value) {
+      if (value[key]) matched.push(key);
+    }
+    value = matched;
+  }
+
+  // ['foo', 'bar', 'baz']
+  if (type(value) === 'array') {
+    if (value.length === 0) {
+      return;
+    }
+    value = value.join(' ');
+  }
+
+  return value;
+}
+
+/**
+ * Events are stored on the node and are creating using
+ * special attributes
+ *
+ * @param {Object} attributes
+ *
+ * @return {Object}
+ */
+
+function parseEvents(attributes) {
+  var ret = {};
+  for (var name in events) {
+    var type = events[name];
+    var callback = attributes[name];
+    if (callback) {
+      ret[type] = callback;
+      delete attributes[name];
+    }
+  }
+  return ret;
+}
+
+/**
+ * Parse the tag to allow using classes and ids
+ * within the tagname like in CSS.
+ *
+ * @param {String} name
+ * @param {Object} attributes
+ *
+ * @return {String}
+ */
+
+function parseTag(name, attributes) {
+  if (!name) return 'div';
+
+  var parts = name.split(/([\.#]?[a-zA-Z0-9_:-]+)/);
+  var tagName = 'div';
+
+  parts
+    .filter(Boolean)
+    .forEach(function(part, i){
+      var type = part.charAt(0);
+      if (type === '.') {
+        attributes.class = ((attributes.class || '') + ' ' + part.substring(1, part.length)).trim();
+      } else if (type === '#') {
+        attributes.id = part.substring(1, part.length);
+      } else {
+        tagName = part;
+      }
+    });
+
+  return tagName;
+}
+},{"component-type":23}],43:[function(_require,module,exports){
+module.exports = TextNode;
+
+/**
+ * Initialize a new `TextNode`.
+ *
+ * This is just a virtual HTML text object.
+ *
+ * @param {String} text
+ * @api public
+ */
+
+function TextNode(text) {
+  this.type = 'text';
+  this.data = String(text);
+}
+},{}],44:[function(_require,module,exports){
+arguments[4][15][0].apply(exports,arguments)
+},{"dup":15}]},{},[1])(1)
 });
