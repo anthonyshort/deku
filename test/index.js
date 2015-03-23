d57dc09f7510ccd2ad47abb652a0477c3bbbb8a7
Anthony Short <antshort@gmail.com>
2015-03-22 23:24:43 -0700

    Cleaned tests

--- test/index.js
+++ test/index.js
@@ -5,20 +5,19 @@ describe('component', function(){
 
 describe('render', function () {
   require('./dom')
-  require('./dom/render-hook');
+  require('./dom/render-hook')
   require('./dom/mount-hook')
   require('./dom/update-hook')
-  require('./dom/props');
-  require('./dom/props');
-  require('./dom/update-elements');
-  require('./dom/update-text');
-  require('./dom/update-attributes');
-  require('./dom/update-replace');
-  require('./dom/events');
-  require('./dom/state');
-});
+  require('./dom/props')
+  require('./dom/elements')
+  require('./dom/text')
+  require('./dom/attributes')
+  require('./dom/update-replace')
+  require('./dom/events')
+  require('./dom/state')
+})
 
 describe('renderString', function () {
   require('./string')
-});
+})
 
