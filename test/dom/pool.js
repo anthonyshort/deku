d0d1a493e0acf198db212139836cf7cd1ee1e211
Anthony Short <antshort@gmail.com>
2015-03-24 23:13:39 -0700

    Added skeleton for tests

---
+++ test/dom/pool.js
@@ -0,0 +1,15 @@
+import assert from 'assert'
+import {component,dom,scene} from '../../'
+import {mount} from '../helpers'
+
+it('should pool dom nodes', function(){
+
+})
+
+it('should pool dom nodes when there are text nodes in between', function(){
+
+})
+
+it('should empty the pools when the renderer is unmounted', function(){
+
+})
