305b17f4fcbf1d2b6341cd565fd25319f34d0933
Anthony Short <antshort@gmail.com>
2015-03-27 09:48:02 -0700

    Added passing tests for pooling

--- test/dom/pool.js
+++ test/dom/pool.js
@@ -2,14 +2,77 @@ import assert from 'assert'
 import {component,dom,scene} from '../../'
 import {mount} from '../helpers'
 
+// Helpers.
+
+function render(props) {
+  var attrs = {};
+  if (props.attr) attrs[props.attr] = props.value;
+  return dom('div#foo', [
+    dom(props.type, attrs, [
+      'boo',
+      dom('strong', attrs),
+      'bar'
+    ])
+  ])
+}
+
+// Tests.
+
 it('should pool dom nodes', function(){
+  var mounted;
+  var Component = component(render)
 
+  var app = scene(Component)
+    .setProps({ type: 'div', attr: 'foo', value: 'bar' })
+
+  mount(app, function(el, renderer){
+    mounted = renderer
+
+    // Switch the nodes back and forth to trigger the pooling
+    var target = el.querySelector('#foo div')
+    var deepTarget = el.querySelector('strong')
+
+    app.replaceProps({ type: 'span' })
+    renderer.render()
+    app.replaceProps({ type: 'div' })
+    renderer.render()
+
+    var next = el.querySelector('#foo div')
+    var deepNext = el.querySelector('strong')
+
+    // It should re-use the same div for the update
+    assert.equal(target, next)
+
+    // It should re-use deeply nested nodes
+    assert.equal(deepTarget, deepNext)
+
+    // It should have placed the span back in the pool
+    assert.equal(renderer.pools.span.storage.length, 1)
+
+    // It should remove attributes from nodes when they are re-used
+    assert(next.hasAttribute('foo') === false)
+    assert(deepNext.hasAttribute('foo') === false)
+  })
+
+  // It should empty the pools when unmounted so that elements don't hang
+  // around in the cache forever.
+  assert.deepEqual(mounted.pools, {})
 })
 
-it('should pool dom nodes when there are text nodes in between', function(){
+it('should not pool components that have the option disabled', function () {
+  var Component = component(render)
+    .set('disablePooling', true)
 
-})
+  var app = scene(Component)
+    .setProps({ type: 'div' })
 
-it('should empty the pools when the renderer is unmounted', function(){
-
-})
+  mount(app, function(el, renderer){
+    var target = el.querySelector('#foo div')
+    app.replaceProps({ type: 'span' })
+    renderer.render()
+    app.replaceProps({ type: 'div' })
+    renderer.render()
+    var next = el.querySelector('#foo div')
+    assert(target !== next)
+  })
+});
