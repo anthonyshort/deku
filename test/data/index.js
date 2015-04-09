71fc90bf786fab4d93a26fe2e4b3f614c96d19c4
Lance Pollard <lancejpollard@gmail.com>
2015-04-09 12:22:08 -0700

    changed io event to update

--- test/data/index.js
+++ test/data/index.js
@@ -24,3 +24,28 @@ it('should get default value from data value', function(){
     app.value('meta', { title: 'Hello World' });
   }
 });
+
+it('should update with new value from data source', function(){
+  var Test = component(template)
+    .prop('text', value('meta').pick('title'));
+
+  var app = deku()
+    .set('renderImmediate', true)
+    .source('meta', metaSource);
+
+  var el = div();
+
+  app.mount(el, Test);
+  assert.equal(el.innerHTML, '<div>Hello World</div>');
+  debugger;
+  app.value('meta', { title: 'Hello Pluto' });
+  assert.equal(el.innerHTML, '<div>Hello Pluto</div>');
+
+  function template(props) {
+    return dom('div', {}, props.text);
+  }
+
+  function metaSource(app) {
+    app.value('meta', { title: 'Hello World' });
+  }
+});
