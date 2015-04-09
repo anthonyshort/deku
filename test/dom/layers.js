b08a971d36eb8dde87d39df1c345fc5b6ad21f19
Lance Pollard <lancejpollard@gmail.com>
2015-04-09 15:49:22 -0700

    added entity.id to layer so it will work with diffing

--- test/dom/layers.js
+++ test/dom/layers.js
@@ -3,7 +3,7 @@ import {component,dom,deku} from '../../'
 import assert from 'assert'
 import {mount,div} from '../helpers'
 
-it('render multiple layers', function(){
+it('mount multiple layers', function(){
   var Page = component()
     .layer('main', mainLayer)
     .layer('dialog', dialogLayer);
@@ -27,6 +27,35 @@ it('render multiple layers', function(){
 
   function dialogLayer(props) {
     return dom(Dialog, { message: 'A dialog' });
+  }
+});
+
+it('update multiple layers', function(){
+  var Page = component()
+    .layer('main', mainLayer)
+    .layer('dialog', dialogLayer);
+
+  var Dialog = component(function(props){
+    return dom('div', {}, [ props.message ]);
+  });
+
+  var app = deku().set('renderImmediate', true);
+  var dialog = div();
+  var main = div();
+  app.layer('dialog', dialog);
+  app.layer('main', main);
+  app.mount(Page);
+  assert.equal(main.innerHTML, '<div>A page</div>');
+  assert.equal(dialog.innerHTML, '<div>A dialog</div>');
+  app.update({ text: 'An updated dialog!' });
+  assert.equal(dialog.innerHTML, '<div>An updated dialog!</div>');
+
+  function mainLayer(props) {
+    return dom('div', {}, [ 'A page' ]);
+  }
+
+  function dialogLayer(props) {
+    return dom(Dialog, { message: props.text || 'A dialog' });
   }
 });
 
