57229e459c80708ed0e62ed7ca22480e57fe82ce
Lance Pollard <lancejpollard@gmail.com>
2015-04-08 18:08:39 -0700

    added initial idea for layers

---
+++ test/dom/layers.js
@@ -0,0 +1,32 @@
+
+import {component,dom,deku} from '../../'
+import assert from 'assert'
+import {mount,div} from '../helpers'
+
+it('render multiple layers', function(){
+  var Page = component()
+    .layer('main', mainLayer)
+    .layer('overlay', overlayLayer);
+
+  var Dialog = component(function(props){
+    return dom('div', {}, [ props.message ]);
+  });
+
+  var app = deku().set('renderImmediate', true);
+  var dialog = div();
+  var main = div();
+  app.layer('dialog', dialog);
+  app.layer('main', main);
+  app.mount(main, Page);
+  assert.equal(main.innerHTML, '<div>A page</div>');
+  assert.equal(dialog.innerHTML, '<div>A dialog</div>');
+
+  function mainLayer(props) {
+    return dom('div', {}, [ 'A page' ]);
+  }
+
+  function overlayLayer(props) {
+    return dom(Dialog, { message: 'A dialog' });
+  }
+});
+
