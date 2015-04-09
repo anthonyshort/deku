9625ccede1ee6c96e4bc7f30f4c40d24af79d0ca
Lance Pollard <lancejpollard@gmail.com>
2015-04-09 12:01:27 -0700

    commented back in tests

--- test/index.js
+++ test/index.js
@@ -1,27 +1,27 @@
 
-// require('./virtualize')
+require('./virtualize')
 
-// describe('component', function(){
-//   require('./component')
-// })
+describe('component', function(){
+  require('./component')
+})
 
-// describe('render', function(){
-//   require('./dom')
-//   require('./dom/mount-hook')
-//   require('./dom/update-hook')
-//   require('./dom/props')
-//   require('./dom/elements')
-//   require('./dom/text')
-//   require('./dom/attributes')
-//   require('./dom/replace')
-//   require('./dom/events')
-//   require('./dom/state')
-//   require('./dom/pool')
-// })
+describe('render', function(){
+  require('./dom')
+  require('./dom/mount-hook')
+  require('./dom/update-hook')
+  require('./dom/props')
+  require('./dom/elements')
+  require('./dom/text')
+  require('./dom/attributes')
+  require('./dom/replace')
+  require('./dom/events')
+  require('./dom/state')
+  require('./dom/pool')
+})
 
-// describe('renderString', function(){
-//   require('./string')
-// })
+describe('renderString', function(){
+  require('./string')
+})
 
 describe('data', function(){
   require('./data')
