5a1f4e64a88dccd869c56174b2cc8ce5b8232705
Lance Pollard <lancejpollard@gmail.com>
2015-04-16 15:59:03 -0700

    removed io and value, just made super simple source

--- test/data/index.js
+++ test/data/index.js
@@ -1,15 +1,16 @@
 
-import {component,dom,deku,value} from '../../';
+import {component,dom,deku} from '../../';
+import trigger from 'trigger-event'
 import {div} from '../helpers';
 import assert from 'assert';
 
 it('should get default value from data value', function(){
   var Test = component(template)
-    .prop('text', value('meta').pick('title'));
+    .prop('data', { source: 'meta' });
 
   var app = deku()
     .set('renderImmediate', true)
-    .source('meta', metaSource);
+    .source('meta', { title: 'Hello World' });
 
   var el = div();
 
@@ -17,34 +18,26 @@ it('should get default value from data value', function(){
   assert.equal(el.innerHTML, '<div>Hello World</div>');
 
   function template(props) {
-    return dom('div', {}, props.text);
-  }
-
-  function metaSource(app) {
-    app.value('meta', { title: 'Hello World' });
+    return dom('div', {}, props.data.title);
   }
 });
 
 it('should update with new value from data source', function(){
   var Test = component(template)
-    .prop('text', value('meta').pick('title'));
+    .prop('text', { source: 'title' });
 
   var app = deku()
     .set('renderImmediate', true)
-    .source('meta', metaSource);
+    .source('title', 'Hello World');
 
   var el = div();
 
   app.mount(el, Test);
   assert.equal(el.innerHTML, '<div>Hello World</div>');
-  app.value('meta', { title: 'Hello Pluto' });
+  app.value('title', 'Hello Pluto');
   assert.equal(el.innerHTML, '<div>Hello Pluto</div>');
 
   function template(props) {
     return dom('div', {}, props.text);
-  }
-
-  function metaSource(app) {
-    app.value('meta', { title: 'Hello World' });
   }
 });
