anthonyshort/deku

commit 45d7a7f643e58b3cef011a778aeea0968a3708f6

resolve remaining linter errors

lib/render.js
@@ -122,8 +122,8 @@ function render (app, container, opts) {
    */
 
   function onupdate (name, data) {
-    if (!connections[name]) return;
-    connections[name].forEach(function(update) {
+    if (!connections[name]) return
+    connections[name].forEach(function (update) {
       update(data)
     })
   }
@@ -175,7 +175,7 @@ function render (app, container, opts) {
     trigger('beforeUnmount', entity, [entity.context, entity.nativeElement])
     unmountChildren(entityId)
     removeAllEvents(entityId)
-    var componentEntities = components[entityId].entities;
+    var componentEntities = components[entityId].entities
     delete componentEntities[entityId]
     delete components[entityId]
     delete entities[entityId]
@@ -280,7 +280,6 @@ function render (app, container, opts) {
 
     // Allow rendering again.
     isRendering = false
-
   }
 
   /**
@@ -541,7 +540,6 @@ function render (app, container, opts) {
     // Diff all of the nodes that have keys. This lets us re-used elements
     // instead of overriding them and lets us move them around.
     if (hasKeys) {
-
       // Removals
       forEach(leftKeys, function (leftNode, key) {
         if (rightKeys[key] == null) {
@@ -605,10 +603,8 @@ function render (app, container, opts) {
           )
         }
       })
-
     } else {
       var maxLength = Math.max(prev.children.length, next.children.length)
-
       // Now diff all of the nodes that don't have keys
       for (var i = 0; i < maxLength; i++) {
         var leftNode = prev.children[i]
@@ -740,10 +736,8 @@ function render (app, container, opts) {
       unmountEntity(childId)
       removals.push(path)
     } else {
-
       // Just remove the text node
       if (!isElement(el)) return el && el.parentNode.removeChild(el)
-
       // Then we need to find any components within this
       // branch and unmount them.
       forEach(childrenByPath, function (childId, childPath) {
@@ -889,6 +883,7 @@ function render (app, container, opts) {
         break
       case 'innerHTML':
         el.innerHTML = ''
+        /* falls through */
       case 'value':
         setElementValue(el, null)
         break
@@ -1039,7 +1034,7 @@ function render (app, container, opts) {
    * @param {Entity} entity
    */
 
-  function registerEntity(entity) {
+  function registerEntity (entity) {
     var component = entity.component
     // all entities for this component type.
     var entities = component.entities = component.entities || {}
@@ -1055,7 +1050,7 @@ function render (app, container, opts) {
    * @param {Entity} entity
    */
 
-  function registerSources(entity) {
+  function registerSources (entity) {
     var component = components[entity.id]
     // get 'class-level' sources.
     // if we've already hooked it up, then we're good.
@@ -1163,9 +1158,9 @@ function render (app, container, opts) {
     keypath.set(handlers, [entityId, path, eventType], function (e) {
       var entity = entities[entityId]
       if (entity) {
-        return fn.call(null, e, entity.context, setState(entity))
+        return addEvent(null, e, entity.context, setState(entity))
       } else {
-        return fn.call(null, e)
+        return addEvent(null, e)
       }
     })
   }
@@ -1302,5 +1297,5 @@ function setElementValue (el, value) {
  */
 
 function canSelectText (el) {
-  return el.tagName === 'INPUT' && ['text','search','password','tel','url'].indexOf(el.type) > -1
+  return el.tagName === 'INPUT' && ['text', 'search', 'password', 'tel', 'url'].indexOf(el.type) > -1
 }

lib/stringify.js
@@ -23,7 +23,7 @@ module.exports = function (app) {
     var propTypes = component.propTypes || {}
     var props = defaults(optProps || {}, component.defaultProps || {})
     var state = component.initialState ? component.initialState(props) : {}
-    props.children = children;
+    props.children = children
 
     for (var name in propTypes) {
       var options = propTypes[name]
@@ -119,14 +119,14 @@ function attr (key, val) {
 function isValidAttributeValue (value) {
   var valueType = type(value)
   switch (valueType) {
-  case 'string':
-  case 'number':
-    return true;
+    case 'string':
+    case 'number':
+      return true
 
-  case 'boolean':
-    return value;
+    case 'boolean':
+      return value
 
-  default:
-    return false;
+    default:
+      return false
   }
 }

commit 8c503e28f29797c19da36943294e071a27d0e2b2

Fixed bug with adding events
The addEvent method was broken in 45d7a7f.
This more or less reverts that change.

lib/render.js
@@ -1158,9 +1158,9 @@ function render (app, container, opts) {
     keypath.set(handlers, [entityId, path, eventType], function (e) {
       var entity = entities[entityId]
       if (entity) {
-        return addEvent(null, e, entity.context, setState(entity))
+        return fn(e, entity.context, setState(entity))
       } else {
-        return addEvent(null, e)
+        return fn(e)
       }
     })
   }