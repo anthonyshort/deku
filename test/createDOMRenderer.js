252c6b7dd03f52bc35258d8d75e2416a3ef4d7a0
Anthony Short <antshort@gmail.com>
2015-12-20 10:52:53 -0800

    Added failing test for createDOMRenderer

--- test/createDOMRenderer.js
+++ test/createDOMRenderer.js
@@ -3,26 +3,21 @@ import test from 'tape'
 import {createDOMRenderer} from '../src/createDOMRenderer'
 import h from '../src/element'
 
-test.skip('changing the root element type', t => {
+test.only('rendering elements', t => {
   let el = document.createElement('div')
   let render = createDOMRenderer(el)
-  render(<div/>)
-  let rootEl = el.firstChild
-  render(<div>Hello Pluto</div>)
-  t.equal(el.firstChild, rootEl, 'not replaced')
-  render(<span>Foo!</span>)
-  t.notEqual(el.firstChild, rootEl, 'replaced')
-  t.end()
-})
 
-test.skip('rendering elements', t => {
-  let el = document.createElement('div')
-  let render = createDOMRenderer(el)
   render(<span />)
-  t.equal(el.innerHTML, '<span></span>', 'no attribute')
+  t.equal(el.innerHTML, '<span></span>', 'rendered')
+
+  render(<span name="Tom" />)
+  t.equal(el.innerHTML, '<span name="Tom"></span>', 'attributed added')
+
   render(<div><span /></div>)
-  t.equal(el.innerHTML, '<div><span></span></div>')
+  t.equal(el.innerHTML, '<div><span></span></div>', 'root replaced')
+
   render(<div><div /></div>)
-  t.equal(el.innerHTML, '<div><div></div></div>')
+  t.equal(el.innerHTML, '<div><div></div></div>', 'child replaced')
+
   t.end()
 })
