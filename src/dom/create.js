e6b1a0a33f8266333964b9d7574ed9c1a9f7d728
0a- <a@0a.io>
2016-02-01 06:46:43 +0800

    Reconstruct createElement so the side effects are now accessible

--- src/dom/create.js
+++ src/dom/create.js
@@ -7,37 +7,71 @@ import isNumber from '@f/is-number'
 import isNull from '@f/is-null'
 const cache = {}
 
-/**
- * Create a real DOM element from a virtual element, recursively looping down.
- * When it finds custom elements it will render them, cache them, and keep going,
- * so they are treated like any other native element.
- */
+export default function createElement(vnode, path, dispatch, context) {
+  let DOM =  createWithSideEffects(vnode, path, dispatch, context)
+  runSideEffects(DOM.sideEffects)
+  return DOM.element
+}
 
-export function createElement (vnode, path, dispatch, context) {
-  switch (vnode.type) {
-    case 'text':
-      return createTextNode(vnode.nodeValue)
-    case 'empty':
-      return getCachedElement('noscript')
-    case 'thunk':
-      return createThunk(vnode, path, dispatch, context)
-    case 'native':
-      return createHTMLElement(vnode, path, dispatch, context)
+export function createElementThenEvents(vnode, path, dispatch, context) {
+  let DOM =  createWithSideEffects(vnode, path, dispatch, context)
+  runSideEffects(DOM.sideEffects, {noEventListeners: true, })
+  return {
+    DOMnode: DOM.element,
+    attachEvents: function(PreRenderedElement){
+      runSideEffects(DOM.sideEffects, {onlyEventListeners: true}, PreRenderedElement)
+    }
   }
 }
 
-function getCachedElement (type) {
-  let cached = cache[type]
-  if (isUndefined(cached)) {
-    cached = cache[type] = createNativeElement(type)
+/**
+/* Recursively traverse a tree of side effects created by `createWithSideEffects`, and run each side effect.
+ * Passing a third parameter DOMElement and it will traverse the DOMElement as well.
+ *
+ * A tree of side effects either takes in the value `null` or is a JSON object in the form
+ * {ofParent : P, ofChildren : C}
+ * where P is either an empty array or an array of functions
+ * and C is either an empty array or an array of trees of side effects
+ *
+ */
+
+function runSideEffects(sideEffects, option, DOMElement){
+  if(sideEffects){
+    sideEffects.ofParent.map(function(sideEffect){ sideEffect(option, DOMElement) })
+    sideEffects.ofChildren.map(function(child,index){
+      if(DOMElement){
+        runSideEffects(child, option, DOMElement.childNodes[index])
+      }else{
+        runSideEffects(child, option)
+      }
+    })
   }
-  return cached.cloneNode(false)
+}
+
+/**
+ * Create a DOM element with a tree of side effects from a virtual element by recursion.
+ * When it finds custom elements it will render them, cache them, and keep going,
+ * so they are treated like any other native element.
+ *
+ */
+
+export function createWithSideEffects (vnode, path, dispatch, context) {
+  switch (vnode.type) {
+    case 'text':
+      return {element: createTextNode(vnode.nodeValue), sideEffects: null}
+    case 'empty':
+      return {element: getCachedElement('noscript'), sideEffects: null}
+    case 'thunk':
+      return {element: createThunk(vnode, path, dispatch, context), sideEffects: null}
+    case 'native':
+      return {element: createHTMLElement(vnode, path, dispatch, context), sideEffects: null}
+  }
 }
 
 function createTextNode (text) {
   let value = isString(text) || isNumber(text)
-    ? text
-    : ''
+      ? text
+      : ''
   return document.createTextNode(value)
 }
 
@@ -63,19 +97,28 @@ function createThunk (vnode, path, dispatch, context) {
 }
 
 function createHTMLElement (vnode, path, dispatch, context) {
-  let { tagName, attributes, children } = vnode
-  let DOMElement = getCachedElement(tagName)
+  let DOMElement = getCachedElement(vnode.tagName)
+  let sideEffects = {ofParent: [], ofChildren: []}
 
-  for (let name in attributes) {
-    setAttribute(DOMElement, name, attributes[name])
-  }
-
-  children.forEach((node, index) => {
-    if (isNull(node) || isUndefined(node)) return
-    let childPath = createPath(path, node.key || index)
-    let child = createElement(node, childPath, dispatch, context)
-    DOMElement.appendChild(child)
+  let attributes = Object.keys(vnode.attributes)
+  sideEffects.ofParent = attributes.map(function(name){
+    return function(option, element = DOMElement){
+      setAttribute(element, name, vnode.attributes[name], null, option)
+    }
   })
 
-  return DOMElement
+  vnode.children.forEach((node, index) => {
+    if (isNull(node) || isUndefined(node)) return
+    let DOM = createWithSideEffects(
+        node,
+        createPath(path, node.key || index),
+        dispatch,
+        context
+    )
+    sideEffects.ofChildren.push(DOM.sideEffects)
+    
+    DOMElement.appendChild(DOM.element)
+  })
+
+  return {element: DOMElement, sideEffects: sideEffects}
 }
